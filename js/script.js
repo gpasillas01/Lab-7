a03a1bee10e4a732bc7b1cce5de44d785c10a67b
gpasillas01 <60024567+gpasillas01@users.noreply.github.com>
2020-04-21 02:26:12 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,7 +1,9 @@
 function init(){
- eb.addEventListener('click', function() {
-    alert('Griselda Pasillas: ' + msg.value);
-	document.getElementById('textoutput').innerHTML = msg.value;});}
+ 
   var msg = document.getElementById('entryinput');	
-  var eb = document.getElementById('entrybutton');}}
+  var eb = document.getElementById('entrybutton');
+eb.addEventListener('click', function() {
+  alert('Griselda Pasillas: ' + msg.value);
+document.getElementById('textoutput').innerHTML = msg.value;});
+}
 window.addEventListener('load', init);
