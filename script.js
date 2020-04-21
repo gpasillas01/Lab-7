94fa25d2d652972949c326e1f6fdd0a3cbe8ee74
gpasillas01 <60024567+gpasillas01@users.noreply.github.com>
2020-04-21 02:32:22 -0500

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-function init(){
- eb.addEventListener('click', function() {
-    alert('Griselda Pasillas: ' + msg.value);
-	document.getElementById('textoutput').innerHTML = msg.value;});
-  var msg = document.getElementById('entryinput');	
-  var eb = document.getElementById('entrybutton');}
-window.addEventListener('load', init);
